0265aacfb49ee0669aa8ac766ee7bebea1e39db2
hou <houjinting1110@163.com>
2016-04-09 18:30:29 +0800

    第三次

---
+++ app/src/main/java/com/example/hou/myhuanxindemo/MyApplication.java
@@ -0,0 +1,14 @@
+package com.example.hou.myhuanxindemo;
+
+import android.app.Application;
+
+/**
+ * Created by hou on 2016/4/9.
+ */
+public class MyApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
