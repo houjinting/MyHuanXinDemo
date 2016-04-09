86005b11efe676f8db43e454ce59720a99f23828
hou <houjinting1110@163.com>
2016-04-09 18:28:55 +0800

    第二次

---
+++ app/src/main/java/com/example/hou/myhuanxindemo/MyView.java
@@ -0,0 +1,22 @@
+package com.example.hou.myhuanxindemo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by hou on 2016/4/9.
+ */
+public class MyView extends View{
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
